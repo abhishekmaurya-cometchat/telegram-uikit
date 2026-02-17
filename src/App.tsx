db49ddd78ef46e6f8b97f2e89b5a7d31338cd568
abhishekmaurya-cometchat <abhishek.maurya@cometchat.com>
2026-02-17 22:58:46 +0530

    Added button to check if Sentry is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { CometChatUIKit } from "@cometchat/chat-uikit-react";
+import * as Sentry from "@sentry/react";
 import { Login } from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import { ChatContext } from "./context/ChatContext";
@@ -14,8 +15,32 @@ export default function App() {
       .then((user) => setLoggedInUser(user))
   }, []);
 
+  const testSentry = () => {
+    Sentry.captureMessage("Sentry test message - it works!");
+    throw new Error("Sentry test error - check your Sentry dashboard!");
+  };
+
   return (
     <BrowserRouter>
+      {/* Sentry Test Button - Remove after confirming it works */}
+      <button
+        onClick={testSentry}
+        style={{
+          position: "fixed",
+          bottom: 20,
+          right: 20,
+          zIndex: 9999,
+          padding: "10px 20px",
+          background: "#362d59",
+          color: "white",
+          border: "none",
+          borderRadius: 8,
+          cursor: "pointer",
+        }}
+      >
+        Test Sentry
+      </button>
+
       <Routes>
         <Route
           path="/"
